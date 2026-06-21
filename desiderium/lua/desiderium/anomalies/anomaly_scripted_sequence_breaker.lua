68ca175dcdbfbd2ff1135b4da582b0599c462ad5
gabriel <kleincolombo@gmail.com>
2026-06-21 01:14:04 -0300

    Delete desiderium/lua/desiderium/anomalies/anomaly_scripted_sequence_breaker.lua

--- desiderium/lua/desiderium/anomalies/anomaly_scripted_sequence_breaker.lua
+++
@@ -1,9 +0,0 @@
--- Disabled: Scripted Sequence Breaker
--- This file was intentionally disabled and replaced by the Breaker Unit anomaly.
--- Keeping a stub here so old references don't error.
-
-if SERVER then
-	print("[DESIDERIUM] anomaly_scripted_sequence_breaker disabled.")
-end
-
-return
